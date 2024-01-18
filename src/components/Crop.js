56f2275155227963a210b31411f3b5a7942aba9f
goto <momomo@xcreed.net>
2024-01-18 20:02:59 +0900

    fix to run

--- src/components/Crop.js
+++ src/components/Crop.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import { Component } from 'react'
 import PropTypes from 'prop-types'
 import { equals, is, update, remove } from 'ramda'
 import interact from 'interactjs'
--- src/components/MultiCrops.js
+++ src/components/MultiCrops.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import { Component } from 'react'
 import { both, clone, is, complement, equals, map, addIndex } from 'ramda'
 import PropTypes from 'prop-types'
 import shortid from 'shortid'
